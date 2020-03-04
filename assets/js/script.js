2cb76a210685a777a659fd09d03b5d32becf75a3
Dmitri Dmitri <dbouilov09@gmail.com>
2020-03-03 19:58:15 -0500

    some basic JS and events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 console.log("hello")
+$(document).ready(function(){
+
+
+    $("#mealTypeSelection").on("click", function(event){
+
+        var mealTypeSelection = event.target.id
+
+        console.log(mealTypeSelection)
+        })
+
+        
+})
+
+
